a1a0c36ac5fe724f9733e624a50eefcd9e37b1f1
Daniel Lemire <lemire@gmail.com>
2017-03-03 11:17:00 -0500

    Formatted

--- src/main/java/me/lemire/microbenchmarks/rangequery/RangeMinimum.java
+++ src/main/java/me/lemire/microbenchmarks/rangequery/RangeMinimum.java
@@ -15,64 +15,64 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class RangeMinimum {
-	final static int N = 1000000;
-	final static int t = 100;
+    final static int N = 1000000;
+    final static int t = 100;
     static int[] b = new int[N];
     static int[] m = new int[t];
     static int[] M = new int[t];
 
     @Setup
     public void populate() {
-      System.out.println("populating...");
-      Random r = new Random();
-      for (int i = 0; i < N; i++){
-                b[i] = r.nextInt();
-      }
-      for (int i = 0; i < t; i++){
-                m[i] = r.nextInt(N - 1);
-                M[i] = r.nextInt(N - m[i] - 1) + m[i] + 1;
-      }
-      System.out.println("populating... ok");
+        System.out.println("populating...");
+        Random r = new Random();
+        for (int i = 0; i < N; i++) {
+            b[i] = r.nextInt();
+        }
+        for (int i = 0; i < t; i++) {
+            m[i] = r.nextInt(N - 1);
+            M[i] = r.nextInt(N - m[i] - 1) + m[i] + 1;
+        }
+        System.out.println("populating... ok");
 
     }
 
 
     @Benchmark
     public int embeddedmin() {
-      int sum = 0;
-      for (int i = 0; i < t; i++) {
-    	  final int minrange = m[i];
-    	  final int maxrange = M[i];
-    	  int thismin = b[minrange];
-    	  for(int j = minrange + 1; j < maxrange; j ++) {
-    		  if(thismin < b[j]) thismin = b[j];
-    	  }
-    	  sum += thismin;
-      }
-	  return sum;
+        int sum = 0;
+        for (int i = 0; i < t; i++) {
+            final int minrange = m[i];
+            final int maxrange = M[i];
+            int thismin = b[minrange];
+            for(int j = minrange + 1; j < maxrange; j ++) {
+                if(thismin < b[j]) thismin = b[j];
+            }
+            sum += thismin;
+        }
+        return sum;
     }
 
     @Benchmark
     public int fncmin() {
-      int sum = 0;
-      for (int i = 0; i < t; i++) {
-    	  sum += rangemin(b,m[i], M[i]);
-      }
-	  return sum;
-    }    
+        int sum = 0;
+        for (int i = 0; i < t; i++) {
+            sum += rangemin(b,m[i], M[i]);
+        }
+        return sum;
+    }
 
     static int rangemin(int[] b, int minrange, int maxrange) {
-    	  int thismin = b[minrange];
-    	  for(int j = minrange + 1; j < maxrange; j ++) {
-    		  if(thismin < b[j]) thismin = b[j];
-    	  }
-    	  return thismin;
+        int thismin = b[minrange];
+        for(int j = minrange + 1; j < maxrange; j ++) {
+            if(thismin < b[j]) thismin = b[j];
+        }
+        return thismin;
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .include(RangeMinimum.class.getSimpleName())
-                .warmupIterations(5).measurementIterations(5).forks(1).build();
+        .include(RangeMinimum.class.getSimpleName())
+        .warmupIterations(5).measurementIterations(5).forks(1).build();
 
         new Runner(opt).run();
     }
